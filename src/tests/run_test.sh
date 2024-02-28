adf424cd2d9ef61a2fd630ff7dd41c980f18da9a
Eliott-B <eliottb.info@gmail.com>
2024-02-28 15:54:53 +0100

    Fix du script de test

--- src/tests/run_test.sh
+++ src/tests/run_test.sh
@@ -1,10 +1,10 @@
 # Tests all components
 
-if (-d vendor) then
+if [ -d vendor ]; then
     echo "Vendor directory exists"
 else
     echo "Vendor directory does not exist"
     composer install
+fi
 
 ./vendor/bin/phpunit src/*.php
-
