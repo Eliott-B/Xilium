d12135e97a231600ac27242a98856d8373ef9f1c
444chak <444chak@gmail.com>
2024-02-28 20:47:34 +0100

    afficher les derniers tickets modifiés en premier

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -9,7 +9,7 @@ class IndexController
     public function index()
     {
         $ticket = new Ticket();
-        $tickets = $ticket->all();
+        $tickets = $ticket->custom("select * from tickets where status_id =:id order by update_date desc", ['id' => 1]);
         $tickets = array_slice($tickets, 0, 10);
         require 'views/index.php';
     }
