7962676d261b99b408801ce90e614e39606b7e4c
Junior 78 <alexie78180@gmail.com>
2024-03-21 19:15:04 +0100

    Ajout de la documentation des models

--- src/app/models/Label.php
+++ src/app/models/Label.php
@@ -1,15 +1,31 @@
 <?php
 
 namespace app\models;
-
+/**
+ * Model représentant un label
+ */
 class Label extends Model
 {
+    /**
+     * Le nom de la table associée à ce modèle
+     * @var string
+     */
     protected string $table = 'labels';
+
+    /**
+     * Les attributs pouvant être remplis
+     * @var array
+     */
     protected array $fillable = [
         'lab_name',
         'lab_css_color'
     ];
 
+    /**
+     * Récupère un label à partir de son id
+     * @param int $id L'id du label à récupérer
+     * @return array Les données du label récupéré
+     */
     public function get_label($id)
     {
         $label = new Label();
