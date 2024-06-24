28265e3f6ec75d644fe3eb4bb266bdfa37db6362
444chak <444chak@gmail.com>
2024-06-24 23:02:48 +0200

    🐞 fix: fix header admin colors buttons

--- src/public/views/adminweb_categories_update.php
+++ src/public/views/adminweb_categories_update.php
@@ -5,14 +5,14 @@
     <link rel="stylesheet" href="/css/adminweb.css">
     <div class="btns">
         <button class="btn-secondary" onclick="window.location.href='/admin/users';">Gestion des utilisateurs</button>
-        <button class="btn-primary" onclick="window.location.href='/admin/labels';">Gestion des labels</button>
-        <button class="btn-secondary" onclick="window.location.href='/admin/categories';">Gestion des
-            catégories</button>
+        <button class="btn-secondary" onclick="window.location.href='/admin/labels';">Gestion des labels</button>
+        <button class="btn-primary" onclick="window.location.href='/admin/categories';">Gestion des catégories</button>
     </div>
 
     <div class="update-categories">
         <div class="update-categories-frame">
-            <form action="/admin/categories/update/<?= $category['cat_id'] ?>" method="post" class="update-categories-form">
+            <form action="/admin/categories/update/<?= $category['cat_id'] ?>" method="post"
+                class="update-categories-form">
                 <div class="update-categories-main">
                     <div class="update-categories-main-name">
                         <label for="cat_name">Nom de la catégorie</label>
@@ -20,7 +20,8 @@
                     </div>
                     <div class="update-categories-main-color">
                         <label for="cat_css_color">Couleur</label>
-                        <input type="color" name="cat_css_color" id="cat_css_color" value="<?= $category['cat_css_color'] ?>">
+                        <input type="color" name="cat_css_color" id="cat_css_color"
+                            value="<?= $category['cat_css_color'] ?>">
                     </div>
                 </div>
                 <div class="update-categories-form-actions">
