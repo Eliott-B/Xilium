90148ab69a98873f57e606bc5a748ddb4c42404b
444chak <444chak@gmail.com>
2024-06-24 23:02:48 +0200

    🐞 fix: fix header admin colors buttons

--- src/public/views/adminweb_categories_create.php
+++ src/public/views/adminweb_categories_create.php
@@ -5,9 +5,8 @@
     <link rel="stylesheet" href="/css/adminweb.css">
     <div class="btns">
         <button class="btn-secondary" onclick="window.location.href='/admin/users';">Gestion des utilisateurs</button>
-        <button class="btn-primary" onclick="window.location.href='/admin/labels';">Gestion des labels</button>
-        <button class="btn-secondary" onclick="window.location.href='/admin/categories';">Gestion des
-            catégories</button>
+        <button class="btn-secondary" onclick="window.location.href='/admin/labels';">Gestion des labels</button>
+        <button class="btn-primary" onclick="window.location.href='/admin/categories';">Gestion des catégories</button>
     </div>
 
     <div class="create-categories">
